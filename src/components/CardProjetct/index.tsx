b00996c0035d6ad10b71765dddb1acb53e48ffca
Leandro-Oliveira-2 <leandro.oliveira@dcx.ufpb.br>
2025-06-26 11:03:14 -0300

    Projeto com dashs incorporados, spinners adcionados para garantir que a tela não fique apenas "branca" carregando.

--- src/components/CardProjetct/index.tsx
+++ src/components/CardProjetct/index.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./cardProject.module.css";
 import { Card, Box, Typography, Button } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 type CardProjectProps = {
   id: number;
   name: string;
   logoUrl?: string;
+  users?: [];
   mentions: number;
   chartImgUrl?: string; // opcional para um gráfico como imagem
 };
@@ -15,10 +17,10 @@ export default function CardProject({
   name,
   logoUrl,
   mentions,
+  users,
   chartImgUrl,
 }: CardProjectProps) {
   const navigate = useNavigate();
-
 
   const handleSelect = () => {
     navigate(`/projeto/${id}`);
@@ -47,16 +49,54 @@ export default function CardProject({
       </Box>
 
       <Box className={styles.content}>
-        {chartImgUrl && (
-          <img
-            src={chartImgUrl}
-            alt="Gráfico de menções"
-            className={styles.chart}
-          />
-        )}
+        {/*         {chartImgUrl && ( */}
+        <img
+          src={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s"
+          }
+          alt="Gráfico de menções"
+          className={styles.chart}
+        />
+        {/*         )} */}
       </Box>
 
       <Box className={styles.footer}>
+        <Box className={styles.avatars}>
+          {users.length > 3 ? (
+            <>
+              {users.slice(0, 3).map((user) => (
+                <Avatar
+                  key={user.id}
+                  sx={{
+                    width: 30,
+                    height: 30,
+                    ml: users.indexOf(user) > 0 ? -1 : 0,
+                    border: "2px solid white",
+                  }}
+                >
+                  {user.name[0]}
+                </Avatar>
+              ))}
+              <Avatar sx={{ width: 30, height: 30, ml: -1, bgcolor: "#ccc" }}>
+                +{users.length - 3}
+              </Avatar>
+            </>
+          ) : (
+            users.map((user) => (
+              <Avatar
+                key={user.id}
+                sx={{
+                  width: 30,
+                  height: 30,
+                  ml: users.indexOf(user) > 0 ? -1 : 0,
+                  border: "2px solid white",
+                }}
+              >
+                {user.name[0]}
+              </Avatar>
+            ))
+          )}
+        </Box>
         <Button variant="contained" color="success" onClick={handleSelect}>
           Selecionar
         </Button>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,66 +1,35 @@
 import CardProject from "@/components/CardProjetct";
 import { Layout } from "@/components/Layout";
-import { Box, Grid, Typography } from "@mui/material";
+import { fetchProjects } from "@/services/projetos/projetoService";
+import { Box, Grid, Typography, CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
-  const fakeProjects = [
-    {
-      id: 1,
-      name: "Guga Pet",
-      logoUrl:
-        "https://keekinteligencia.com.br/wp-content/uploads/sites/1649/2024/11/keek_banner_mobile2-1-1.jpg",
-      mentions: 495,
-      chartImgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s",
-    },
-    {
-      id: 2,
-      name: "Bayeux",
-      logoUrl:
-        "https://keekinteligencia.com.br/wp-content/uploads/sites/1649/2024/11/keek_banner_mobile2-1-1.jpg",
-      mentions: 320,
-      chartImgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s",
-    },
-    {
-      id: 3,
-      name: "Projeto 3",
-      logoUrl:
-        "https://keekinteligencia.com.br/wp-content/uploads/sites/1649/2024/11/keek_banner_mobile2-1-1.jpg",
-      mentions: 128,
-      chartImgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s",
-    },
-    {
-      id: 4,
-      name: "Guga Pet",
-      logoUrl:
-        "https://keekinteligencia.com.br/wp-content/uploads/sites/1649/2024/11/keek_banner_mobile2-1-1.jpg",
-      mentions: 495,
-      chartImgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s",
-    },
-    {
-      id: 5,
-      name: "Bayeux",
-      logoUrl:
-        "https://keekinteligencia.com.br/wp-content/uploads/sites/1649/2024/11/keek_banner_mobile2-1-1.jpg",
-      mentions: 320,
-      chartImgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPrGHSpnQgqgPyLc_mKIb7SGHfN7eynQAw0g&s",
-    },
-  ];
+  const [projetos, setProjetos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchProjects()
+      .then(setProjetos)
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <Layout titulo="Tela de Projetos">
       <Box padding={2}>
-        <Grid container spacing={3}>
-          {fakeProjects.map((project) => (
-            <Grid item xs={12} md={6} key={project.id}>
-              <CardProject {...project} />
-            </Grid>
-          ))}
-        </Grid>
+        {loading ? (
+          <Box display="flex" justifyContent="center" alignItems="center" height="300px">
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Grid container spacing={3}>
+            {projetos.map((project) => (
+              <Grid item xs={12} md={6} key={project.id}>
+                <CardProject userName={project.users} {...project || []} />
+              </Grid>
+            ))}
+          </Grid>
+        )}
       </Box>
     </Layout>
   );
